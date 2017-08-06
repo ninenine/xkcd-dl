6d98830c0fec83738eeef979858c9895f25a6617
Davis Wainaina <davisjwn@gmail.com>
2017-08-06 15:26:44 +0300

    First Blood

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace xkcd
+{
+    public class Comic
+    {
+        public string title { get; set; }
+        public string alt { get; set; }
+        public string img { get; set; }
+        public int num { get; set; }
+    }
+
+    public class Program
+    {
+        public const string DownloadDir = "XKCD";
+        public static Comic LatestComic { get; set; }
+        public static void Main(string[] args)
+        {
+            if (!Directory.Exists(DownloadDir))
+            {
+                Console.WriteLine("Creating Download directory....");
+                Directory.CreateDirectory(DownloadDir);
+            }
+            Console.WriteLine("Attempting to download latest comic....");
+            downloadComic("http://xkcd.com/info.0.json", true).Wait();
+
+            for (int num = 1; num <= LatestComic.num; num++)
+            {
+                Console.WriteLine("Attempting to download comic #{0}....", num);
+                var url = "http://xkcd.com/" + num + "/info.0.json";
+                downloadComic(url).Wait();
+            }
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        private static async Task downloadComic(string URL, Boolean latest = false)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "GET";
+            request.Accept = "application/json";
+            request.ContentType = "application/json; charset=utf-8";
+            try
+            {
+                var response = (HttpWebResponse)await request.GetResponseAsync();
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    var result = JsonConvert.DeserializeObject<Comic>(reader.ReadToEnd());
+                    // Set latest Comic
+                    if (latest)
+                        LatestComic = result;
+                    string img_url = result.img;
+                    string file = img_url.Split('/').Last();
+                    string fileUploadPath = DownloadDir + "/" + file;
+                    Console.WriteLine("Downloading comic {0}-{1}:{2}", result.num, result.title, result.img);
+                    using (var client = new HttpClient())
+                    using (var contentStream = await client.GetStreamAsync(img_url))
+                    using (var fileStream = new FileStream(fileUploadPath, FileMode.Create, FileAccess.Write, FileShare.None, 1048576, true))
+                    {
+                        await contentStream.CopyToAsync(fileStream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Download Image failed : {0}--{1}", ex.StackTrace, ex.Message);
+                throw;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # xkcd-dl
 Download all xkcd comics
+
+## Installation
+* Clone the [Git Repository](https://github.com/ninenine/xkcd-dl) and edit with your favorite editor. e.g. Visual Studio, Visual Studio Code
+* Lunch with `F5` or `Ctrl+F5` (The usual way)
+
+## License
+Released under the [MIT License](https://github.com/ninenine/xkcd-dl/blob/master/LICENSE).
+
+[YOUR FEEDBACK](mailto:davisjwn@gmail.com) | [FOLLOW ME](https://twitter.com/davisknuckles)
